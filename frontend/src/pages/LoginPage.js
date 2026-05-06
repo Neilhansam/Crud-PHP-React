2af77e99f1ad8d435f8614c1ef01f8daf28c6343
neilhansam <issaiahneil@gmail.com>
2026-05-06 19:08:45 +0800

    creating the LoginPage

---
+++ frontend/src/pages/LoginPage.js
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./LoginPage.css"; // optional: for styles
+
+const LoginPage = () => {
+  
+
+  return (
+    <div className="login-container">
+      
+    </div>
+  );
+}
+
+export default LoginPage;
