765c5b3a90552ff3e75f29a3fe15e44442cc4240
neilhansam <issaiahneil@gmail.com>
2026-05-07 09:44:51 +0800

    changes on adding input to the database

--- backend/add.php
+++ backend/add.php
@@ -1,18 +1,30 @@
 <?php
 include 'db.php';
 
-if(isset($_POST['submit'])){
-    $first = $_POST['first_name'];
-    $middle = $_POST['middle_name'];
-    $last = $_POST['last_name'];
+$data = json_decode(file_get_contents("php://input"));
 
-    $sql = "INSERT INTO users (first_name, middle_name, last_name)
-            VALUES ('$first', '$middle', '$last')";
 
-    if($conn->query($sql)){
-        echo "Data inserted!";
-    } else {
-        echo "Error: " . $conn->error;
-    }
+if(
+    empty($data->first_name) ||
+    empty($data->middle_name) ||
+    empty($data->last_name) 
+)
+
+{
+    echo json_decode(["error" => "All forms are required"]);
+    exit;
 }
+
+$stmt = $conn->prepare("INSERT INTO user(first_name, middle_name, last_name) VALUES(?,?,?) ");
+$stmt->bind_param("fml", $data->first_name, $data->middle_name, $data->last_name);
+if ($stmt->execute()){
+    echo json_decode(["message"=>"User inputted succesfully"]);
+
+}
+else {
+    echo json_decode(["error" =>"Failed to add"]);
+}
+
+$stmt->close();
+$conn->close();
 ?>
