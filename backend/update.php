316a985f018129d026fe41e5ce2cc067ec813d46
neilhansam <issaiahneil@gmail.com>
2026-05-07 21:42:11 +0800

    forgot to put ,

--- backend/update.php
+++ backend/update.php
@@ -8,7 +8,7 @@ include 'db.php';
 $data = json_decode(file_get_contents("php://input"));
 
 
-$stmt = $conn->prepare("UPDATE user SET first_name=?, middle_name=?, last_name=? email=? WHERE id=?");
+$stmt = $conn->prepare("UPDATE user SET first_name=?, middle_name=?, last_name=?, email=? WHERE id=?");
 $stmt->bind_param("ssssi", $data->first_name, $data->middle_name, $data->last_name, $data->email, $data->id);
 
 if ($stmt->execute()) {
